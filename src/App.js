37493bf370291b71410fb3fd8c0e70dbae0f638b
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2022-09-21 14:46:46 -0500

    First Draft Adjustments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+      </head>
+      <header className="headshot">
+        <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png" className="headshot-img" alt="headshot" />
       </header>
+      <main>
+      <h1>Pikachu</h1>
+      <h3>Front End Developer</h3>
+      <p>While Pikachu is known for being very reactionary, he has no idea how React works.
+        Behind his cute smile lies powers that could destory this app. We'll blame the bugs on that...Excellent.
+      </p>
+      </main>
+      <footer>
+      <a href="#" class="fa fa-facebook"></a>
+      <a href="#" class="fa fa-instagram"></a>
+      <a href="#" class="fa fa-linkedin"></a>
+
+      </footer>
     </div>
   );
 }
